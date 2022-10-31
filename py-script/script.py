f346d6276a22a535dea189aed750af7986c3c67f
Lesneo <64084644+Lesneo@users.noreply.github.com>
2022-10-31 20:17:43 +0100

    Opti python

--- py-script/script.py
+++ py-script/script.py
@@ -14,100 +14,19 @@ habitantBio = []
 habitantHalal = []
 habitantVegan = []
 habitantCasher = []
-
-for i in range(taille) :
-    for j in range(len(lables)) :
-        aliment = tableSondage[lables[j]][i]
-        nomAliment = tableAliments[tableAliments.alim_code==aliment].alim_nom_fr
-        catAliment = tableAliments[tableAliments.alim_code==aliment].alim_ssssgrp_nom_fr
-        if "bio" in nomAliment.values[0] :
-            bio+=1
-        if "halal" in catAliment.values[0] :
-            halal+=1
-        if "vegan" in nomAliment.values[0] :
-            vegan+=1
-        if "casher" in catAliment.values[0] :
-            casher += 1
-    if bio >= 3 :
-        habitantBio.append(tableSondage.Nom[i])
-    if halal >= 2 :
-        habitantHalal.append(tableSondage.Nom[i])
-    if vegan >= 3 :
-        habitantVegan.append(tableSondage.Nom[i])
-    if casher >= 2 :
-        habitantCasher.append(tableSondage.Nom[i])
-
-    bio = 0
-    halal = 0
-    vegan = 0
-    casher = 0
-    
-#bio
-if habitantBio == [] :
-    bio = "Aucun habitant préfère les aliments Bio"
-else :
-    bio = "Liste des habitants qui préfèrent les aliments Bio : <ul>"
-    for i in habitantBio :
-        bio += "<li>" + i + "</li> "
-    bio += "</ul>"
-print(bio)
-
-#halal
-if habitantHalal == [] :
-    bio = "Aucun habitant préfère les aliments Halal"
-else :
-    bio = "Liste des habitants qui préfèrent les aliments Halal : <ul>"
-    for i in habitantHalal :
-        bio += "<li>" + i + "</li> "
-    bio += "</ul>"
-print(bio)
-
-#vegan
-if habitantVegan == [] :
-    bio = "Aucun habitant préfère les aliments Vegan"
-else :
-    bio = "Liste des habitants qui préfèrent les aliments Vegan : <ul>"
-    for i in habitantVegan :
-        bio += "<li>" + i + "</li> "
-    bio += "</ul>"
-print(bio)
-
-#casher
-if habitantCasher == [] :
-    bio = "Aucun habitant préfère les aliments Casher"
-else :
-    bio = "Liste des habitants qui préfèrent les aliments Casher : <ul>"
-    for i in habitantCasher :
-        bio += "<li>" + i + "</li> "
-    bio += "</ul>"
-print(bio)
-
-
-
 categorie = tableAliments.alim_grp_nom_fr.unique()
 listeCat = []
 for i in categorie :
     listeCat.append([i,0])
-#print(listeCat)
 
+kjLable = tableAliments.columns[tableAliments.columns == "Energie, Règlement UE N° 1169/2011 (kJ/100 g)"] #J
+sugarLabel = tableAliments.columns[tableAliments.columns == "Sucres (g/100 g)"] #S
+agsLabel = tableAliments.columns[tableAliments.columns == "AG saturés (g/100 g)"] #AF
+sodiumLabel = tableAliments.columns[tableAliments.columns == "Sodium (mg/100 g)"] #BI
+fibreLabel = tableAliments.columns[tableAliments.columns == "Fibres alimentaires (g/100 g)"] #AA
+protLabel = tableAliments.columns[tableAliments.columns == "Protéines, N x facteur de Jones (g/100 g)"] #O
+alimHabitant = []
 
-for i in range(taille) :
-    for j in range(len(lables)) :
-        aliment = tableSondage[lables[j]][i]
-        catAliment = tableAliments[tableAliments.alim_code==aliment].alim_grp_code
-        listeCat[catAliment.values[0]-1][1]+=1
-
-
-def takeSecond(elem):
-    return elem[1]
-
-#print(listeCat)
-listeCat.sort(key=takeSecond, reverse=True)
-#print(listeCat)
-print("<br> <h2> Tableau des catégories d'aliments les plus choisis dans le sondage </h2> <br>")
-tableau = '<table class="table table-striped "><thead class="thead-dark"><tr class="table-dark"><th scope="col">Classement</th><th scope="col">Catégorie Aliment</th><th scope="col">Nombre d\'itérations</th></tr></thead><tbody id="content"> </tbody> </table>'
-print(tableau)
-element = document.getElementById("content")
 def createHTML1(ligne, i):
     tr_element = document.createElement('tr')
     th_ligne = document.createElement('th')
@@ -126,8 +45,26 @@ def createHTML1(ligne, i):
     
     return tr_element
 
-for i in range(len(listeCat)) :
-    element.append(createHTML1(listeCat[i], i))
+
+
+def createHTML2(ligne, s, i):
+    tr_element = document.createElement('tr')
+    th_ligne = document.createElement('th')
+    th_ligne.setAttribute("scope","row")
+    first_name_element = document.createElement('td')
+    score_moyen = document.createElement('td')
+    
+
+    th_ligne.innerText = i
+    first_name_element.innerText = s
+    score_moyen.innerText = ligne
+    
+    tr_element.append(th_ligne)
+    tr_element.append(first_name_element)
+    tr_element.append(score_moyen)
+    
+    return tr_element
+
 def calculKj(kj) :
     if kj == "-" :
         return 0
@@ -284,19 +221,27 @@ dictScore = {
     5: "E"
     }
 
-kjLable = tableAliments.columns[tableAliments.columns == "Energie, Règlement UE N° 1169/2011 (kJ/100 g)"] #J
-sugarLabel = tableAliments.columns[tableAliments.columns == "Sucres (g/100 g)"] #S
-agsLabel = tableAliments.columns[tableAliments.columns == "AG saturés (g/100 g)"] #AF
-sodiumLabel = tableAliments.columns[tableAliments.columns == "Sodium (mg/100 g)"] #BI
-fibreLabel = tableAliments.columns[tableAliments.columns == "Fibres alimentaires (g/100 g)"] #AA
-protLabel = tableAliments.columns[tableAliments.columns == "Protéines, N x facteur de Jones (g/100 g)"] #O
-alimHabitant = []
-
 for i in range(taille) :
     alimHabitant.append([])
     for j in range(len(lables)) :
-        score = 0
         aliment = tableSondage[lables[j]][i]
+        #bio vegan casher halal
+        nomAliment = tableAliments[tableAliments.alim_code==aliment].alim_nom_fr
+        catAliment = tableAliments[tableAliments.alim_code==aliment].alim_ssssgrp_nom_fr
+        if "bio" in nomAliment.values[0] :
+            bio+=1
+        if "halal" in catAliment.values[0] :
+            halal+=1
+        if "vegan" in nomAliment.values[0] :
+            vegan+=1
+        if "casher" in catAliment.values[0] :
+            casher+=1
+
+        #catégorie
+        catAliment = tableAliments[tableAliments.alim_code==aliment].alim_grp_code
+        listeCat[catAliment.values[0]-1][1]+=1
+        
+        #score santé
         valeur = tableAliments[kjLable][tableAliments.alim_code==aliment].values[0][0].replace(",",".")
         #print(valeur)
         alimHabitant[i].append(calculKj(valeur))
@@ -313,44 +258,93 @@ for i in range(taille) :
         alimHabitant[i][j] = alimHabitant[i][j]-calculFibre(valeur)
         valeur = tableAliments[protLabel][tableAliments.alim_code==aliment].values[0][0].replace(",",".").replace("<","").replace(" ","")
         alimHabitant[i][j] = alimHabitant[i][j]-calculProt(valeur)
+    
+    #suite bio vegan casher halal
+    if bio >= 3 :
+        habitantBio.append(tableSondage.Nom[i] + " " + tableSondage.Prénom[i])
+    if halal >= 2 :
+        habitantHalal.append(tableSondage.Nom[i] + " " + tableSondage.Prénom[i])
+    if vegan >= 3 :
+        habitantVegan.append(tableSondage.Nom[i] + " " + tableSondage.Prénom[i])
+    if casher >= 2 :
+        habitantCasher.append(tableSondage.Nom[i] + " " + tableSondage.Prénom[i])
+        score = 0
+
+    bio = 0
+    halal = 0
+    vegan = 0
+    casher = 0
+    
+#bio
+if habitantBio == [] :
+    bio = "Aucun habitant préfère les aliments Bio"
+else :
+    bio = "Liste des habitants qui préfèrent les aliments Bio : <ul>"
+    for i in habitantBio :
+        bio += "<li>" + i + "</li> "
+    bio += "</ul>"
+print(bio)
+
+#halal
+if habitantHalal == [] :
+    bio = "Aucun habitant préfère les aliments Halal"
+else :
+    bio = "Liste des habitants qui préfèrent les aliments Halal : <ul>"
+    for i in habitantHalal :
+        bio += "<li>" + i + "</li> "
+    bio += "</ul>"
+print(bio)
+
+#vegan
+if habitantVegan == [] :
+    bio = "Aucun habitant préfère les aliments Vegan"
+else :
+    bio = "Liste des habitants qui préfèrent les aliments Vegan : <ul>"
+    for i in habitantVegan :
+        bio += "<li>" + i + "</li> "
+    bio += "</ul>"
+print(bio)
+
+#casher
+if habitantCasher == [] :
+    bio = "Aucun habitant préfère les aliments Casher"
+else :
+    bio = "Liste des habitants qui préfèrent les aliments Casher : <ul>"
+    for i in habitantCasher :
+        bio += "<li>" + i + "</li> "
+    bio += "</ul>"
+print(bio)
 
 
-#print(alimHabitant)
+#catégorie
+def takeSecond(elem):
+    return elem[1]
 
+listeCat.sort(key=takeSecond, reverse=True)
+print("<br> <h2> Tableau des aliments les plus choisis dans le sondage </h2> <br>")
+tableau = '<table class="table table-striped "><thead class="thead-dark"><tr class="table-dark"><th scope="col">Classement</th><th scope="col">Catégorie Aliment</th><th scope="col">Nombre d\'itérations</th></tr></thead><tbody id="content"> </tbody> </table>'
+print(tableau)
+element = document.getElementById("content")
+for i in range(len(listeCat)) :
+    element.append(createHTML1(listeCat[i], i))
+
+
+#score habitant
 scoreAlim = []
 for i in range(len(alimHabitant)) :
     scoreAlim.append([])
     for j in range(len(alimHabitant[i]))  :
         scoreAlim[i].append(calculScore(alimHabitant[i][j]))
 
-#print(scoreAlim)
-
 scoreHabit = []
 for i in scoreAlim :
     scoreHabit.append(sum(i)/len(i))
 
-#print(scoreHabit)
 print("<br> <h2> Tableau des aliments les plus choisis dans le sondage </h2> <br> <p> Le score est calculé avec la moyenne du Nutriscore des 10 produits : </p> <br>")
 tableau = '<table class="table table-striped "><thead class="thead-dark"><tr class="table-dark"><th scope="col">Habitant</th><th scope="col">Score Santé</th><th scope="col">Score moyen des produits</th></tr></thead><tbody id="contentScore"> </tbody> </table>'
 print(tableau)
 element = document.getElementById("contentScore")
-def createHTML2(ligne, s, i):
-    tr_element = document.createElement('tr')
-    th_ligne = document.createElement('th')
-    th_ligne.setAttribute("scope","row")
-    first_name_element = document.createElement('td')
-    score_moyen = document.createElement('td')
-    
 
-    th_ligne.innerText = i
-    first_name_element.innerText = s
-    score_moyen.innerText = ligne
-    
-    tr_element.append(th_ligne)
-    tr_element.append(first_name_element)
-    tr_element.append(score_moyen)
-    
-    return tr_element
 
 nomLabel = tableSondage.columns[tableSondage.columns == "Nom"]
 prenomLabel = tableSondage.columns[tableSondage.columns == "Prénom"]
